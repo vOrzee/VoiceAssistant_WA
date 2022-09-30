47813430a9177c89f9194d582d6debbdf938245b
vOrzee <104573735+vOrzee@users.noreply.github.com>
2022-10-01 00:28:46 +0500

    Add option menu

--- app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
+++ app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.netology.voiceassistant_wa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    fun initViews() {
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_stop -> {
+                return true
+            }
+            R.id.action_clear -> {
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
