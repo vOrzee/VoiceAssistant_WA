7031f13e6fa9f764008bf64aa2fe47255655422a
vOrzee <104573735+vOrzee@users.noreply.github.com>
2022-10-01 16:15:55 +0500

    Add test pods

--- app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
+++ app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.netology.voiceassistant_wa
 
+import android.content.AbstractThreadedSyncAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.textfield.TextInputEditText
 
@@ -11,7 +14,28 @@ class MainActivity : AppCompatActivity() {
 
     val TAG: String = "MainActivity"
 
-    lateinit var requestInput:TextInputEditText
+    lateinit var requestInput: TextInputEditText
+
+    lateinit var podsAdapter: SimpleAdapter
+
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String, String>().apply {
+            put("Title", "Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 2")
+            put("Content", "Content 2")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 3")
+            put("Content", "Content 3")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 4")
+            put("Content", "Content 4")
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +47,19 @@ class MainActivity : AppCompatActivity() {
     fun initViews() {
         val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        requestInput = findViewById(R.id.text_input_edit)
+
+        val podsList: ListView = findViewById(R.id.pods_list)
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title", "Content"),
+            intArrayOf(R.id.title, R.id.content)
+        )
+        podsList.adapter = podsAdapter
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
