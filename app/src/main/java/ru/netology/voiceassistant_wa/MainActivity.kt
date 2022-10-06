971a9e718e512ffc64173f8d34467f48cc387f2e
Roman Leshin <vorzee@gmail.com>
2022-10-06 14:36:17 +0500

    Refactor test

--- app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
+++ app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
@@ -1,7 +1,5 @@
 package ru.netology.voiceassistant_wa
 
-import android.app.Activity
-import android.content.AbstractThreadedSyncAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -25,22 +23,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var progressBar: ProgressBar
 
     val pods = mutableListOf<HashMap<String, String>>(
-        HashMap<String, String>().apply {
-            put("Title", "Title 1")
-            put("Content", "Content 1")
-        },
-        HashMap<String, String>().apply {
-            put("Title", "Title 2")
-            put("Content", "Content 2")
-        },
-        HashMap<String, String>().apply {
-            put("Title", "Title 3")
-            put("Content", "Content 3")
-        },
-        HashMap<String, String>().apply {
-            put("Title", "Title 4")
-            put("Content", "Content 4")
-        }
+
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,14 +49,13 @@ class MainActivity : AppCompatActivity() {
         )
         podsList.adapter = podsAdapter
         val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
-        var count = 5
         voiceInputButton.setOnClickListener {
 
             Log.d(TAG,"Click on floating button")
             pods.add(
                 HashMap<String, String>().apply {
-                    put("Title", "Title $count")
-                    put("Content", "Content ${count++}")
+                    put("Title", "Title ${pods.size + 1}")
+                    put("Content", "Content ${pods.size + 1}")
                 }
             )
             podsList.adapter = podsAdapter
