d9169419488aed7cfc48b486de40dd62eecb9b08
Roman Leshin <vorzee@gmail.com>
2022-10-06 17:23:19 +0500

    Add options on button stop

--- app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
+++ app/src/main/java/ru/netology/voiceassistant_wa/MainActivity.kt
@@ -1,7 +1,9 @@
 package ru.netology.voiceassistant_wa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.Menu
@@ -56,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
         requestInput = findViewById(R.id.text_input_edit)
         requestInput.setOnEditorActionListener { _, actionId, _ ->
-            if(actionId == EditorInfo.IME_ACTION_DONE) {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
                 pods.clear()
                 podsAdapter.notifyDataSetChanged()
 
@@ -86,7 +88,7 @@ class MainActivity : AppCompatActivity() {
         val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
         voiceInputButton.setOnClickListener {
 
-            Log.d(TAG,"Click on floating button")
+            Log.d(TAG, "Click on floating button")
             pods.add(
                 HashMap<String, String>().apply {
                     put("Title", "Title ${pods.size + 1}")
@@ -107,7 +109,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_stop -> {
-                if (isTtsReady){
+                if (isTtsReady) {
                     textToSpeech.stop()
                 }
                 return true
@@ -122,46 +124,47 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    fun initWolframEngine(){
+    fun initWolframEngine() {
         waEngine = WAEngine().apply {
             appID = "Y7VJAU-L48TAHRGTH"
             addFormat("plaintext")
         }
     }
 
-    fun showSnackbar(message: String){
-        Snackbar.make(findViewById(android.R.id.content),message,Snackbar.LENGTH_INDEFINITE).apply {
-            setAction(android.R.string.ok) {
-                dismiss()
+    fun showSnackbar(message: String) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_INDEFINITE)
+            .apply {
+                setAction(android.R.string.ok) {
+                    dismiss()
+                }
+                show()
             }
-            show()
-        }
     }
 
-    fun askWolfram(request:String){
+    fun askWolfram(request: String) {
         progressBar.visibility = View.VISIBLE
         CoroutineScope(Dispatchers.IO).launch {
             val query = waEngine.createQuery().apply { input = request }
             runCatching {
                 waEngine.performQuery(query)
             }.onSuccess { result ->
-                withContext(Dispatchers.Main){
+                withContext(Dispatchers.Main) {
                     progressBar.visibility = View.GONE
                     if (result.isError) {
                         showSnackbar(result.errorMessage)
                         return@withContext
                     }
 
-                    if(!result.isSuccess) {
+                    if (!result.isSuccess) {
                         requestInput.error = getString(R.string.error_do_not_understand)
                         return@withContext
                     }
-                    for (pod in result.pods){
+                    for (pod in result.pods) {
                         if (pod.isError) continue
                         val content = StringBuilder()
-                        for (subpod in pod.subpods){
-                            for (element in subpod.contents){
-                                if(element is WAPlainText){
+                        for (subpod in pod.subpods) {
+                            for (element in subpod.contents) {
+                                if (element is WAPlainText) {
                                     content.append(element.text)
                                 }
                             }
@@ -174,7 +177,7 @@ class MainActivity : AppCompatActivity() {
                     podsAdapter.notifyDataSetChanged()
                 }
             }.onFailure { t ->
-                withContext(Dispatchers.Main){
+                withContext(Dispatchers.Main) {
                     progressBar.visibility = View.GONE
                     showSnackbar(t.message ?: getString(R.string.error_something_went_wrong))
                 }
@@ -182,9 +185,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun initTts(){
+    fun initTts() {
         textToSpeech = TextToSpeech(this) { code ->
-            if(code != TextToSpeech.SUCCESS){
+            if (code != TextToSpeech.SUCCESS) {
                 showSnackbar(getString(R.string.error_tts_is_not_ready) + ". Error code: $code")
             } else {
                 isTtsReady = true
@@ -193,4 +196,5 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
 }
